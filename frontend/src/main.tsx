7aa83e4f433bb336841a337c69195bbb38432541
Hakeemnk18 <hakeemnk18@gmail.com>
2025-07-19 08:53:29 +0530

    login button completed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import allRoutes from './route/Index.tsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './store/store.ts'
 
 
 const appRouter = createBrowserRouter(allRoutes)
@@ -11,8 +13,12 @@ const root = createRoot(document.getElementById('root')!)
 
 
 root.render(
+  <Provider store={store}>
+
+  
   <StrictMode>
     <RouterProvider router={appRouter} />
   </StrictMode>
+  </Provider>
 )
 
